39c83a6c806f2aee77ab64b11fdcb258628427ea
olame <olame>
2015-04-21 02:52:04 +0300

    initial commit

---
+++ VeloPins/VeloPins/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  VeloPins
+//
+//  Created by Olga Grineva on 21/04/15.
+//  Copyright (c) 2015 Olga Grineva. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
